18f14ce4c9f1f7035131d1783386daeb5f813721
yuanxin <yuanxin.qiu@easyread.ly>
2014-01-05 19:05:48 +0800

    build base file structure

---
+++ app/controllers/documents_controller.rb
@@ -0,0 +1,30 @@
+class DocumentsController < ApplicationController
+
+  # GET /documents
+  def index
+  end
+
+  # GET /documents/:id
+  def show
+  end
+
+  # GET /documents/new
+  def new
+  end
+
+  # POST /documents
+  def create
+  end
+
+  # GET /documents/:id/edit
+  def edit
+  end
+
+  # PATCH /documents/:id
+  def update
+  end
+
+  # DELETE /documents/:id
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Apidoc::Application.routes.draw do
   devise_for :users
+
+  resources :documents
+
+  root "documents#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
