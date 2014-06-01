2ae023900d71da9d6bb4ccc9e50216facd01e441
yuanxin.qiu <yuanxin.qiu@easyread.ly>
2014-06-01 21:19:21 +0800

    Build javascript mvc file structure

---
+++ app/assets/javascripts/mvc/models/category.js
@@ -0,0 +1,8 @@
+var Sdm = window.Sdm || {};
+
+var Sdm.Models.Category = Backbone.Model.extend({
+
+  initialize: function() {
+  }
+
+});
---
+++ app/assets/javascripts/mvc/sdm.js
@@ -0,0 +1,13 @@
+//= require_self
+//= require_tree ./templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
+
+window.Sdm = {
+  Models: {},
+  Collections: {},
+  Routers: {},
+  Views: {}
+};
